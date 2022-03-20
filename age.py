94114ba27a586dcc492b1fd02283a7dc6111318d
Prashantha <8rkprashanth@gmail.com>
2022-03-20 13:49:04 +0530

    new scripts are added

---
+++ age.py
@@ -0,0 +1,4 @@
+age=int(input("how old are you in decade?\n"))
+decade=age//10
+year=age % 10
+print(str(decade) + " decade " + str(year) + " years" ) 
--- tax.py
+++ tax.py
@@ -2,3 +2,5 @@ amount = 10
 tax =   0.6
 total = amount + amount*tax
 print(total)
+myname = input("what is your name?\n")
+print(myname)
